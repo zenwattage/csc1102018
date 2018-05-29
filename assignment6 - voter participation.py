00d745ee2d9e0cdea3798d8d9017e17c4168a4d3
zenwattage <zenwattage@gmail.com>
2018-05-29 09:20:32 -0700

    origin

--- assignment6 - voter participation.py
+++ assignment6 - voter participation.py
@@ -28,37 +28,91 @@
 
 #get file to open from user
 user = input('Enter the name of your data file: ')
-
-#open and read the file
-infile = open(user,'r')
-line = infile.readline()
-
-#new output file name begins with 'REPORT-'
 new_file = 'REPORT-' + user
+
+#lists
+years = []
+eli_voters = []
+reg_voters = []
+num_ballots = []
+
 years_total = 0
 ballots_total = 0
 
-for i in infile:
-    print(line)
-    line = infile.readline()
+#OPEN AND READ FILE
+infile = open(user,'r')
+line = infile.readline().strip('\n')
 
-    #open 'REPORT-' file
+
+
+
+
+# while line != '':
+#             line = weatherFile.readline()
+#             month.append(line)
+#             line = weatherFile.readline()
+#             num = int(line)
+#             total = 0
+#             numRain = 0
+#             days.append(num)
+#             for i in range(num):
+#                 line = weatherFile.readline()
+#                 temp.append(int(line))
+#                 total += int(line) 
+#                 line = weatherFile.readline()
+#                 if(int(line))== 1:
+#                     numRain += 1
+#                     rain.append(numRain)
+#                     avgTemp.append(total / num)
+#             for i in range(len(month)):
+#                 print(month[i],end = "")
+
+
+
+
+
+while line != '':
+    year = line
+    #assign lines to lists
+    #the year line
+    year_line = infile.readline().strip('\n')
+    years.append(year_line)
+
+    years_total += int(year_line)
+
+    #eligible voters that year
+    eli_line = infile.readline().strip('\n')
+    eli_voters.append(eli_line)
+
+    #registered voters that year
+    reg_voters_line = infile.readline().strip('\n')
+    reg_voters.append(reg_voters_line)
+
+    #number of ballots cast that year
+    num_ballots_line = infile.readline()
+    num_ballots.append(num_ballots_line)
+
+
+
+    # print(years)
+    # print(eli_voters)
+    # print(reg_voters)
+    # print(num_ballots)
+
+    #% of registered voters
+    # LINE 3 / LINE 2 * 100
+    regVoters = (int(reg_voters_line) / int(eli_line))*100
     
+    #% who actually voted
+    # LINE 4 / LINE 1 * 100
+    percVoted = (int(num_ballots_line) / int(eli_line))*100
 
+    #print statement
     #write data lines to file in format:
     #In 1952, 90.81% registered and 72.80% voted.
+    print('In '+ year_line +', ' + format(regVoters,'.2f') + '% ' +\
+    'registered and ' + format(percVoted,'.2f') + '% ' + 'voted.')
     
-
-    # % of registered voters
-    # LINE 3 / LINE 2 * 100
-
-    # % voted
-    # LINE 4 / LINE 1 * 100
-
-
-  
-
-
     #line = infile.readline()
 
 
